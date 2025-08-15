376528102b93d807403db91f70be2c450cfa51a6
osgsm <50955473+osgsm@users.noreply.github.com>
2025-08-15 09:44:21 +0900

    Change clear color

--- src/pages/05/_App.ts
+++ src/pages/05/_App.ts
@@ -144,7 +144,7 @@ export class App {
   setupRendering = () => {
     const gl = this.gl;
     gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-    gl.clearColor(0.3, 0.3, 0.3, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
   };
 
